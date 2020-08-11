021e0fee81e3f73c4eae04e4b4aff6b163641ad2
Samora Machel <samoraok@gmail.com>
2020-08-11 09:31:45 +0300

    Added a crawler to the profile page

--- Utilities/botException.py
+++ Utilities/botException.py
@@ -1,7 +1,14 @@
-class MissingWeDriver(Exception):
+import logging
+import datetime
+
+logging.basicConfig(filename="../BotIn.log", level=logging.INFO)
+class MissingWebDriver(Exception):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.message = args[0]
+        logging.error(f"{self.message} at {datetime.datetime.now()}")
+        
 
     def __str__(self):
-        return f("{self.message} \n--> Please define a webdriver ")
+        return (f"{self.message} \n--> Please define a webdriver ")
+
